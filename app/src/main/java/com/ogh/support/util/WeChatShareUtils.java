ebfe00b0ccebff5755cc7810341a7019fceb9453
OneGreenHand <295268305@qq.com>
2020-11-16 19:07:24 +0800

    新增微信图片分享、去除过时方法

--- app/src/main/java/com/ogh/support/util/WeChatShareUtils.java
+++ app/src/main/java/com/ogh/support/util/WeChatShareUtils.java
@@ -8,11 +8,14 @@ import com.ogh.support.AppContext;
 import com.frame.util.ToastUtil;
 import com.ogh.support.R;
 import com.tencent.mm.opensdk.modelmsg.SendMessageToWX;
+import com.tencent.mm.opensdk.modelmsg.WXImageObject;
 import com.tencent.mm.opensdk.modelmsg.WXMediaMessage;
 import com.tencent.mm.opensdk.modelmsg.WXTextObject;
 import com.tencent.mm.opensdk.modelmsg.WXWebpageObject;
 import com.tencent.mm.opensdk.openapi.IWXAPI;
 import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * 微信分享工具类
@@ -22,7 +25,7 @@ public class WeChatShareUtils {
     /**
      * 分享微信文本
      *
-     * @param type 1: 朋友圈  2: 好友对话
+     * @param type 0：分享到好友  1：分享到朋友圈
      */
     public static void shareWeChatTxt(String content, int type) {
         WXTextObject textObject = new WXTextObject();
@@ -33,7 +36,7 @@ public class WeChatShareUtils {
         SendMessageToWX.Req req = new SendMessageToWX.Req();
         req.transaction = "";
         req.message = mediaMessage;
-        req.scene = type == 1 ? SendMessageToWX.Req.WXSceneTimeline : SendMessageToWX.Req.WXSceneSession;
+        req.scene = type;
         String appId = AppContext.getContext().getResources().getString(R.string.wx_appid);
         IWXAPI iwxapi = WXAPIFactory.createWXAPI(FrameApplication.getContext(), appId);
         if (!iwxapi.isWXAppInstalled()) {
@@ -47,7 +50,7 @@ public class WeChatShareUtils {
     /**
      * 发起app网页分享
      *
-     * @param type 1: 朋友圈  2: 好友对话
+     * @param type 0：分享到好友  1：分享到朋友圈
      */
     public static void shareWeChatUrl(String title, String desc, String url, int type) {
         //初始化一个WXWebpageObject，填写url
@@ -64,7 +67,7 @@ public class WeChatShareUtils {
         SendMessageToWX.Req req = new SendMessageToWX.Req();
         req.transaction = "";
         req.message = mediaMessage;
-        req.scene = type == 1 ? SendMessageToWX.Req.WXSceneTimeline : SendMessageToWX.Req.WXSceneSession;
+        req.scene = type;
         String appId = AppContext.getContext().getResources().getString(R.string.wx_appid);
         IWXAPI iwxapi = WXAPIFactory.createWXAPI(FrameApplication.getContext(), appId);
         if (!iwxapi.isWXAppInstalled()) {
@@ -73,5 +76,41 @@ public class WeChatShareUtils {
         }
         iwxapi.registerApp(appId);
         iwxapi.sendReq(req);
+    }
+
+    /**
+     * 发起图片分享
+     *
+     * @param type 0：分享到好友  1：分享到朋友圈
+     */
+    public static void shareWeChatImg(Bitmap bitmap, int type) {
+        WXImageObject imgObj = new WXImageObject(bitmap);
+        WXMediaMessage msg = new WXMediaMessage();
+        msg.mediaObject = imgObj;
+        Bitmap thumbBmp = Bitmap.createScaledBitmap(bitmap, 150, 150, true);
+        bitmap.recycle();
+        msg.thumbData = bmpToByteArray(thumbBmp);//设置缩略图
+        SendMessageToWX.Req req = new SendMessageToWX.Req();
+        req.transaction = buildTransaction("img");
+        req.message = msg;
+        req.scene = type;
+        String appId = AppContext.getContext().getResources().getString(R.string.wx_appid);
+        IWXAPI iwxapi = WXAPIFactory.createWXAPI(FrameApplication.getContext(), appId);
+        if (!iwxapi.isWXAppInstalled()) {
+            ToastUtil.showShortToast("未安装微信,请安装后再尝试分享");
+            return;
+        }
+        iwxapi.registerApp(appId);
+        iwxapi.sendReq(req);
+    }
+
+    private static byte[] bmpToByteArray(Bitmap bm) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        return baos.toByteArray();
+    }
+
+    private static String buildTransaction(final String type) {
+        return (type == null) ? String.valueOf(System.currentTimeMillis()) : type + System.currentTimeMillis();
     }
 }

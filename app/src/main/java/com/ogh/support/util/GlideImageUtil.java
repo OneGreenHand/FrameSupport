81bb7b493d045e52e0885bd46950928481ee09ff
OneGreenHand <295268305@qq.com>
2021-09-18 14:07:53 +0800

    修改build

--- app/src/main/java/com/ogh/support/util/GlideImageUtil.java
+++ app/src/main/java/com/ogh/support/util/GlideImageUtil.java
@@ -70,8 +70,6 @@ public class GlideImageUtil {
         Glide.with(context)
                 .asBitmap()
                 .load(url)
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .skipMemoryCache(true)
                 .into(new SimpleTarget<Bitmap>() {
                     @Override
                     public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
